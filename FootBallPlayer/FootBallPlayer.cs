eadf10a54dcdd0df6392872c9573ca3196f2c36d
chald80 <72771464+chald80@users.noreply.github.com>
2021-09-29 12:40:12 +0200

    Add project files.

---
+++ FootBallPlayer/FootBallPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Opgave1
+{
+    public class FootBallPlayer
+    {
+
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+
+        public FootBallPlayer(int id, string name, int price, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => value = _id;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "Name is null");
+                if (value.Length < 4) throw new ArgumentException("Name length must be at least 4 characters");
+                _name = value;
+            }
+        }
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value > 0) _price = value;
+                else throw new ArgumentOutOfRangeException("Price", value, "Illegal");
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (1 <= value && value <= 100) _shirtNumber = value;
+                else throw new ArgumentOutOfRangeException("Shirtnumber", value, "du er en hat");
+            }
+        }
+
+    }
+}
