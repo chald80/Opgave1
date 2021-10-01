de010da7f51a0f6c3c45a52ab2da065aad6e66fc
chald80 <72771464+chald80@users.noreply.github.com>
2021-10-01 09:28:11 +0200

    update

--- FootBallPlayerTests/FootBallPlayerTests.cs
+++ FootBallPlayerTests/FootBallPlayerTests.cs
@@ -1,20 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Opgave1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Opgave1.Tests
+namespace TestFootBallPlayer
 {
-    [TestClass()]
-    public class FootBallPlayerTests
+    [TestClass]
+    public class TestPlayer
     {
-        [TestMethod()]
-        public void FootBallPlayerTest()
+        private FootBallPlayer _footBallPlayer;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            Assert.Fail();
+            _footBallPlayer = new FootBallPlayer(1, "Anders", 3000, 50);
+        }
+
+        [TestMethod]
+        public void TestNameConstructor()
+        {
+            FootBallPlayer fbp = new FootBallPlayer(1, "Anders", 3000, 50);
+            //Assert.AreEqual(1, fbp.Id);
+            Assert.AreEqual("Anders", fbp.Name);
+            Assert.AreEqual(3000, fbp.Price);
+            Assert.AreEqual(50, fbp.ShirtNumber);
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+            _footBallPlayer.Name = "Anders";
+            Assert.AreEqual("Anders", _footBallPlayer.Name);
+            Assert.ThrowsException<ArgumentNullException>(() => _footBallPlayer.Name = null);
+            Assert.ThrowsException<ArgumentException>(() => _footBallPlayer.Name = "Bob");
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            _footBallPlayer.Price = 100;
+            Assert.AreEqual(100, _footBallPlayer.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footBallPlayer.Price = 0);
+
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
